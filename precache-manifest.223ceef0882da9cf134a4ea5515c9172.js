9e99913d00c997a684a7760273a6317543ae01ba
Karol <kovalczyk97@gmail.com>
2020-10-07 12:12:50 +0200

    Updates

---
+++ precache-manifest.223ceef0882da9cf134a4ea5515c9172.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "929f3410b1722b4fffce1f3e390d4365",
+    "url": "/user-auth-frontend/index.html"
+  },
+  {
+    "revision": "17e0577ef94034feda6a",
+    "url": "/user-auth-frontend/static/css/main.8ea52bc9.chunk.css"
+  },
+  {
+    "revision": "6e8956e42da9b85b610f",
+    "url": "/user-auth-frontend/static/js/2.c91f1f63.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/user-auth-frontend/static/js/2.c91f1f63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17e0577ef94034feda6a",
+    "url": "/user-auth-frontend/static/js/main.525136dd.chunk.js"
+  },
+  {
+    "revision": "a4893b8aacbad5fc4662",
+    "url": "/user-auth-frontend/static/js/runtime-main.14f43652.js"
+  }
+]);
